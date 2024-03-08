1f381ccb09197916fcf97d117307dfc7f0f45b57
faom002 <faisal_jobb@outlook.com>
2024-03-08 19:00:27 +0100

    test

--- week4/headers/RectangleComponent.h
+++ week4/headers/RectangleComponent.h
@@ -9,26 +9,21 @@
 struct RectangleComponent : public Component {
   void draw_rectangle_component() override {
     float speed = 50.0f; // set my choice for how fast it should go
-    float deltaTime = 20.0f;
+    float deltaTime = 21.0f;
 
     float accumulator;
 
-
-
     while (accumulator >= speed) {
 
-    accumulator += deltaTime;
-      // Calculate color components using sine waves to create a smooth color
-      // transition
+      accumulator += deltaTime;
       unsigned char r = static_cast<unsigned char>(accumulator);
       unsigned char g = static_cast<unsigned char>(accumulator);
       unsigned char b = static_cast<unsigned char>(accumulator);
       unsigned char a = static_cast<unsigned char>(accumulator);
 
-      ClearBackground(WHITE); // Clear the background
+      ClearBackground(WHITE);
 
-      DrawRectangle(1200 / 2, 800 / 2, 500, 500,
-                    Color{r, g, b, a}); // Draw the rectangle
+      DrawRectangle(1200 / 2, 800 / 2, 500, 500, Color{r, g, b, a});
 
       accumulator -= speed;
     }
